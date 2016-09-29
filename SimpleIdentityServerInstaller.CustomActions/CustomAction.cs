e96c0953872381299c18cfea5c1f9cf470609820
thabart <habarthierry@hotmail.fr>
2016-09-29 20:24:19 +0200

    Ticket 204 : Update json + start to create the second screen

--- SimpleIdentityServerInstaller.CustomActions/CustomAction.cs
+++ SimpleIdentityServerInstaller.CustomActions/CustomAction.cs
@@ -1,5 +1,9 @@
 using Microsoft.Deployment.WindowsInstaller;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SimpleIdentityServerInstaller.CustomActions
 {
@@ -10,8 +14,12 @@ namespace SimpleIdentityServerInstaller.CustomActions
         {
             try
             {
+                System.Diagnostics.Debugger.Launch();
                 session.Log("Start to update the appsettings.json files");
-
+                // TODO = Replace the current path
+                const string currentPath = @"c:\Project\SimpleIdServerInstaller\sources";
+                var appSettings = new string[] { Path.Combine(currentPath, @"SimpleIdentityServer\SimpleIdentityServer.Startup\appsettings.json") };
+                UpdateConnectionStrings(appSettings, session, "connectionstring");
                 session.Log("Finish to update the appsettings.json files");
             }
             catch(Exception ex)
@@ -21,6 +29,37 @@ namespace SimpleIdentityServerInstaller.CustomActions
             }
 
             return ActionResult.Success;
+        }
+
+        private static void UpdateConnectionStrings(IEnumerable<string> paths, Session session, string connectionString)
+        {
+            foreach(var path in paths)
+            {
+                if (!File.Exists(path) && path.EndsWith("appsettings.json"))
+                {
+                    continue;
+                }
+
+                var json = File.ReadAllText(path);
+                var jObj = JObject.Parse(json);
+                var token = jObj.SelectToken("Data.DefaultConnection.ConnectionString");
+                if (token == null)
+                {
+                    session.Log("the connection string doesn't exist");
+                    continue;
+                }
+
+                var val = token as JValue;
+                if (val == null)
+                {
+                    session.Log("it's not a JValue");
+                    continue;
+                }
+
+                val.Value = connectionString;
+                var output = jObj.ToString(Formatting.None);
+                File.WriteAllText(path, output);
+            }
         }
     }
 }
