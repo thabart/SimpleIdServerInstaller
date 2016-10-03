0df0cfe3e9ec6f789fd41da327d7f8bd54cb8907
thabart <habarthierry@hotmail.fr>
2016-10-03 19:33:59 +0200

    Ticket 204 : Token url is now correctly set

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 echo restore the nuget packages ...
+set SERVER=simpleidserver
 dotnet restore repositories/SimpleIdentityServer/SimpleIdentityServer/src && ^
 dotnet publish repositories/SimpleIdentityServer/SimpleIdentityServer/src/SimpleIdentityServer.Startup -f net46 -o sources/SimpleIdentityServer/SimpleIdentityServer.Startup && ^
 dotnet publish repositories/SimpleIdentityServer/SimpleIdentityServer/src/SimpleIdentityServer.Uma.Host -f net46 -o sources/SimpleIdentityServer/SimpleIdentityServer.Uma.Host && ^
@@ -9,6 +10,6 @@ dotnet publish repositories/SimpleIdentityServer/SimpleIdentityServer/src/Simple
 dotnet publish repositories/SimpleIdentityServer/SimpleIdentityServer/src/SimpleIdentityServer.Configuration.IdServer.Startup -f net46 -o sources/SimpleIdentityServer/SimpleIdentityServer.Configuration.IdServer.Startup" && ^
 npm install repositories/UmaManagerWebSiteApi --prefix repositories/UmaManagerWebSiteApi && ^
 echo f | XCOPY repositories\UmaManagerWebSiteApi sources\UmaManagerWebSiteApi\ /D /E /Y && ^
-cd repositories\UmaManagerWebSite && npm install && bower install && set SERVER=simpleidserver && ember build && ^
+cd repositories\UmaManagerWebSite && npm install && bower install && ember build && ^
 cd ..\..\ && echo f | XCOPY repositories\UmaManagerWebSite\dist sources\UmaManagerWebSite\dist\ /D /E /Y && ^
 cd sources\UmaManagerWebSite\ && npm install && cd ..\..\
--- sources/UmaManagerWebSite/server.js
+++ sources/UmaManagerWebSite/server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('dist'));
 
+app.get('*', function(req, res) {
+    res.sendfile('./dist/index.html'); // load our public/index.html file
+});
+
 app.listen(4200, function () {
   console.log('Example app listening on port 4200!');
 });
