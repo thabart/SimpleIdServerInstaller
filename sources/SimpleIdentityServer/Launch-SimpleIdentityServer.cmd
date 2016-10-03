964c0f232dab4b6496a88834af61ed249cd7d7d0
thabart <habarthierry@hotmail.fr>
2016-10-03 12:51:14 +0200

    Ticket 204 : Add environment variable

--- sources/SimpleIdentityServer/Launch-SimpleIdentityServer.cmd
+++
@@ -1,8 +0,0 @@
-REM for /d /r . %%d in (bin,obj) do @if exist "%%d" rd /s/q "%%d"
-set ASPNETCORE_ENVIRONMENT=
-set DATA_MIGRATED=true
-START cmd /k "cd SimpleIdentityServer.Startup && SimpleIdentityServer.Startup.exe --server.urls=https://*:5443"
-START cmd /k "cd SimpleIdentityServer.Uma.Host && SimpleIdentityServer.Uma.Host.exe --server.urls=https://*:5445"
-START cmd /k "cd SimpleIdentityServer.Manager.Host.Startup && SimpleIdentityServer.Manager.Host.Startup.exe --server.urls=http://*:5002"
-START cmd /k "cd SimpleIdentityServer.Configuration.Startup && SimpleIdentityServer.Configuration.Startup.exe --server.urls=http://*:5004"
-echo Applications are running ...
